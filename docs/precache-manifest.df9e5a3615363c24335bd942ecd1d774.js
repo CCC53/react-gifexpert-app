813663361d09ac7fd766531de45dc92f65346b85
Javier Calette <javiercalette@gmail.com>
2020-09-03 13:59:27 -0500

    Docs ready

---
+++ docs/precache-manifest.df9e5a3615363c24335bd942ecd1d774.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b06688f3bf464c01fac09dbbf501a1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98e91c6264322a1d69e1",
+    "url": "/static/css/main.7aefbcfd.chunk.css"
+  },
+  {
+    "revision": "601b0d55f32663f658c7",
+    "url": "/static/js/2.7f357064.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7f357064.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98e91c6264322a1d69e1",
+    "url": "/static/js/main.57b7777b.chunk.js"
+  },
+  {
+    "revision": "39e88660e5d512526393",
+    "url": "/static/js/runtime-main.3c337214.js"
+  }
+]);
